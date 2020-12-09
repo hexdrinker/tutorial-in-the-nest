d65d59b4acc33c815b5aa4b06714110ed3352c27
박영호 <mellonia.bluerose@gmail.com>
2020-12-09 21:50:47 +0900

    remove controller, service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 // Decorator, 클래스에 함수 기능을 추가함.
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
